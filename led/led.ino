14e194694b402dc4a1e535eeae15e7d93628cbb4
Rahat-CS <69900347+Rahat-CS@users.noreply.github.com>
2023-07-13 13:13:42 +0600

    Add files via upload

---
+++ led/led.ino
@@ -0,0 +1,31 @@
+const int trigPin = 2;
+const int echoPin = 3;
+const int LED=4;
+int distance ;
+long duration;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigPin,OUTPUT);//sets the trigPin as an Output
+  pinMode(echoPin,INPUT);//sets the echoPin as an Input
+  pinMode(LED,OUTPUT);
+  Serial.begin(9600);//Starts the serial communication
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite (trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+  duration=pulseIn(echoPin,HIGH);
+  distance=duration*0.034/2;
+  Serial.print("Distance:");
+  Serial.println(distance);
+  if(distance<20)
+  digitalWrite(LED,HIGH);
+  else
+  digitalWrite(LED,LOW);
+  delay(500);
+}
